fe340da8182b62af11e6c9d5aed19f069d2e95f4
Suhyun Kim <kim00431@algonquinlive.com>
2025-02-09 22:42:52 -0500

    jquery practice

--- js/common.js
+++ js/common.js
@@ -69,7 +69,7 @@ const footer_menu = () => {
     // 모든 요소에서 'active' 제거
     footer_titles.forEach((item) => item.classList.remove("active"));
   }
-}
+};
 
 // const footer_menu4 = () => {
 //     const footer_titles = document.querySelectorAll(".fnb > li");
@@ -79,14 +79,13 @@ const footer_menu = () => {
 //         e.preventDefault();
 //         console.log('clicked')
 //         reset_menu(title)
-//         let hasActive = title.classList.contains('active'); 
+//         let hasActive = title.classList.contains('active');
 //         console.log('before toggle', hasActive)
 //         title.classList.toggle('active')
 //         console.log('after toggle', hasActive)
 //       });
 //     });
-  
-  
+
 //     function reset_menu(activeMenu) {
 //       footer_titles.forEach(menu => {
 //           // 클릭된 메뉴와 동일하지 않은 경우에만 'on' 제거
@@ -95,7 +94,7 @@ const footer_menu = () => {
 //           }
 //       });
 //       }
-     
+
 //   };
 
 // const footer_menu = () => {
@@ -158,10 +157,69 @@ const open_tablet = () => {
   });
 };
 
+const number = () => {
+  const num1 = document.querySelector(".num1");
+  let count1 = parseInt(num1.textContent);
+
+  const num2 = document.querySelector(".num2");
+  let count2 = parseInt(num2.textContent);
+
+  const num3 = document.querySelector(".num3");
+  let count3 = parseInt(num3.textContent);
+
+  function increase1() {
+    const overCheck = count1 < 63;
+    if (overCheck) {
+      if (count1 < 60) {
+        count1 += 10;
+      } else {
+        count1 += 1;
+      }
+      num1.textContent = count1;
+    } else {
+    }
+  }
+  function increase2() {
+    const overCheck = count2 < 288;
+    if (overCheck) {
+      if (count2 < 200) {
+        count2 += 100;
+      } else if (count2 < 260) {
+        count2 += 10;
+      } else {
+        count2 += 1;
+      }
+      num2.textContent = count2;
+    }
+  }
+
+  function increase3() {
+    const overCheck = count3 < 319;
+    if (overCheck) {
+      if (count3 < 300) {
+        count3 += 100;
+      } else if (count3 < 310) {
+        count3 += 10;
+      } else {
+        count3 += 1;
+      }
+      num3.textContent = count3;
+    }
+  }
+  setInterval(increase1, 100);
+  setTimeout(() => {
+    setInterval(increase2, 50);
+  }, 900);
+  setTimeout(() => {
+    setInterval(increase3, 100);
+  }, 1500);
+};
+
 document.body.addEventListener("htmx:afterSwap", () => {
   footer_menu();
   header_main_btn();
   // theme();
   search();
   open_tablet();
+  number();
 });
