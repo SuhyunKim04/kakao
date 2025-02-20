6ddb22eac0b7570461b9b614b67a34d607079501
Suhyun Kim <kim00431@algonquinlive.com>
2025-02-20 00:29:41 -0500

    updates

--- js/slide.js
+++ js/slide.js
@@ -3,16 +3,16 @@ $(function () {
     const $item_1 = $(".item_1");
     const $item_2 = $(".item_2");
     $item_1.click(function () {
-      $(".inner_slide_1").fadeIn(300, function () {
-        $(".inner_slide_2").fadeOut();
+      $(".slide_item_1").fadeIn(300, function () {
+        $(".slide_item_2").fadeOut();
       });
       $(this).addClass("active");
       $item_2.removeClass("active");
     });
 
     $item_2.click(function () {
-      $(".inner_slide_2").fadeIn();
-      $(".inner_slide_1").fadeOut();
+      $(".slide_item_2").fadeIn();
+      $(".slide_item_1").fadeOut();
       $(this).addClass("active");
       $item_1.removeClass("active");
     });
