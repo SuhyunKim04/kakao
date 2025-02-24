d212f900d7b84366cd9401aed5daf642b5948497
Suhyun Kim <kim00431@algonquinlive.com>
2025-02-23 21:47:20 -0500

    cards

--- js/slide.js
+++ js/slide.js
@@ -1,5 +1,26 @@
 $(function () {
-  function cardFadeMotion() {
+  function cardFadeMotion(container) {
+    const items = $(`${container} .item`);
+    const buttons = $(`${container} .slide_button button`);
+
+    buttons.each(function (index, btn) {
+      console.log(btn);
+
+      btn.click(function () {
+        console.log("hello", items[index]);
+        console.log(items.eq(index));
+        //버튼의 0번째를 누르면 슬라이드의 0번째 인덱스 번호가 slidein이 되야함
+      });
+    });
+
+    /*
+
+    items.forEach( function(item,index) {})
+    items.forEach( (item,index) => {})
+      */
+
+    /*
+
     const $item_1 = $(".item_1");
     const $item_2 = $(".item_2");
     $item_1.click(function () {
@@ -16,7 +37,8 @@ $(function () {
       $(this).addClass("active");
       $item_1.removeClass("active");
     });
+    */
   }
 
-  cardFadeMotion();
+  cardFadeMotion(".slide_container");
 });
